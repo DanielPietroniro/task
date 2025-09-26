432a7f39b71d272e97e6461da9916cfaad283039
Daniel Pietroniro <pietroniro.daniel@gmail.com>
2025-09-26 15:00:36 -0400

    commit

---
+++ build/cpppackage/CMakeFiles/cpppackage_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpppackage_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/cpppackage_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/cpppackage/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniel/task/task/cpppackage
+# Build directory: /home/daniel/task/task/build/cpppackage
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
